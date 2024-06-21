d8f9d7bde9735ba2c3b8f33625629e4487dbddbe
artemijspavlovs <19916123+artemijspavlovs@users.noreply.github.com>
2024-06-21 20:07:31 +0300

    ci: apply updated templates

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ Copyright © 2023 Artemijs Pavlovs <hi@artpav.dev>
 */
 package main
 
-import "github.com/artemijspavlovs/gopipeit/cmd"
+import "github.com/artemijspavlovs/gopipeit/v2/cmd"
 
 func main() {
 	cmd.Execute()
